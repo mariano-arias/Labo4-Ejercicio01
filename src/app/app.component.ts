48b7e3746053e61fe7c88227902b4bd7975ad7f7
mariano-arias <marianoluisarias@gmail.com>
2023-03-30 22:24:23 -0300

    ejercicio 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'firstApp';
+
+  title = 'Ejercicio 01';
+  edadUno: number = 0;
+  edadDos: number = 0;
+  suma: number = 0;
+  promedio: number = 0;
+
+  Calcular() {
+    this.suma = this.edadUno + this.edadDos;
+    this.promedio = this.suma / 2;
+  }
+
+
+  Clean() {
+    this.edadUno = 0;
+    this.edadDos = 0;
+    this.promedio = 0;
+    this.suma = 0;
+
+  }
+
+
 }
